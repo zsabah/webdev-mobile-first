e2b77ad6d99ef9b2ad6fb387fd5be4731bf26177
martin@mustbebuilt.co.uk <martinmustbebuilt@gmail.com>
2023-10-24 11:16:19 +0100

    Set up 2023

--- js/main.js
+++ js/main.js
@@ -1,34 +1,33 @@
-(function () {
-  let setMenu = function () {
-    // variables
-    let burgerMenu = document.querySelector(".burger");
-    let navBar = document.querySelector("nav");
+(() => {
+  const initBurgerMenu = () => {
+    const burgerMenu = document.querySelector(".burger");
+    const navBar = document.querySelector("nav");
     let navBarStatus = false;
-    navBar.removeAttribute("style");
-    // mobile menu via CSS Animation
-    if (window.getComputedStyle(navBar).overflow == "hidden") {
-      let navHeight = navBar.offsetHeight;
+
+    if (window.getComputedStyle(navBar).overflow === "hidden") {
+      const navHeight = navBar.offsetHeight;
       navBar.style.height = "0px";
-      burgerMenu.addEventListener("click", () => {
-        console.info(burgerMenu.classList);
+
+      const toggleMenu = () => {
         if (navBarStatus) {
           navBarStatus = false;
           navBar.style.height = "0px";
-          // burger animation class
-          burgerMenu.setAttribute("class", "burger");
+          burgerMenu.classList.remove("changeBurger");
         } else {
           navBarStatus = true;
           navBar.style.height = navHeight + "px";
-          // burger animation class
-          burgerMenu.setAttribute("class", "burger changeBurger");
+          burgerMenu.classList.add("changeBurger");
         }
-      });
-    } else {
-      navBarStatus = false;
-      navBar.removeAttribute("style");
+      };
+
+      burgerMenu.addEventListener("click", toggleMenu);
     }
   };
-  setMenu();
-  // resize
-  window.addEventListener("resize", () => setMenu());
+
+  const handleResize = () => {
+    initBurgerMenu();
+  };
+
+  initBurgerMenu();
+  window.addEventListener("resize", handleResize);
 })();
